4792e78e48c9ccbb11db9fc850b79459553bcbb0
ggadwa <ggadwa@2c4a6428-2a2a-0410-ad13-7dde452bfe2e>
2010-02-19 21:48:34 +0000

    02_19_2010 PC

--- dim3Engine/Sources_Network/net_client_join_host.c
+++ dim3Engine/Sources_Network/net_client_join_host.c
@@ -63,122 +63,22 @@ int net_client_find_game(char *game_name)
 
 /* =======================================================
 
-      Ping Host
-      
-======================================================= */
-
-bool net_client_ping_host(char *ip,char *status,char *host_name,char *proj_name,char *game_name,char *map_name,int *player_count,int *player_max_count,int *ping_msec)
-{
-	int						msec,count;
-	char					err_str[256];
-	bool					connect_ok;
-	network_reply_info		reply_info;
-	
-	status[0]=0x0;
-	game_name[0]=0x0;
-	map_name[0]=0x0;
-	*player_count=0;
-	*player_max_count=0;
-	*ping_msec=-1;
-	
-		// create client socket
-		
-	client_socket=net_open_tcp_socket();
-	if (client_socket==D3_NULL_SOCKET) {
-		strcpy(status,"Unreachable");
-		return(FALSE);
-	}
-	
-		// connect to host
-		
-	if (!net_connect_start(client_socket,ip,net_port_host,err_str)) {
-		strcpy(status,"Unreachable");
-		net_close_socket(&client_socket);
-		return(FALSE);
-	}
-	
-		// check if connection completed
-		// do it in a non-blocking manner so the join
-		// UI can exit if needed
-		
-	count=client_timeout_wait_seconds;
-	connect_ok=FALSE;
-	
-	count*=1000;
-	
-	while ((count>0) && (!join_thread_quit)) {
-	
-		if (net_connect_check(client_socket)) {
-			connect_ok=TRUE;
-			break;
-		}
-	
-		usleep(1000);
-		count--;
-	}
-
-	net_connect_end(client_socket);
-	
-		// connection failed?
-		
-	if (!connect_ok) {
-		strcpy(status,"Unreachable");
-		net_close_socket(&client_socket);
-		return(FALSE);
-	}
-	
-		// query for info
-		
-	msec=time_get();
-	
-	if (!net_queue_block_single_message(client_socket,net_action_request_info,net_remote_uid_none,NULL,0,net_action_reply_info,(unsigned char*)&reply_info,sizeof(network_reply_info))) {
-		strcpy(status,"Unreachable");
-		net_close_socket(&client_socket);
-		return(FALSE);
-	}
-
-	msec=time_get()-msec;
-	if (msec<1) msec=1;
-	
-		// close socket
-		
-	net_close_socket(&client_socket);
-	
-		// ok status message
-		
-	strcpy(status,"OK");
-	
-	strncpy(host_name,reply_info.host_name,name_str_len);
-	host_name[name_str_len-1]=0x0;
-	strncpy(proj_name,reply_info.proj_name,name_str_len);
-	proj_name[name_str_len-1]=0x0;
-	strncpy(game_name,reply_info.game_name,name_str_len);
-	game_name[name_str_len-1]=0x0;
-	strncpy(map_name,reply_info.map_name,name_str_len);
-	map_name[name_str_len-1]=0x0;
-
-	*player_count=(signed short)ntohs(reply_info.player_count);
-	*player_max_count=(signed short)ntohs(reply_info.player_max_count);
-	*ping_msec=msec;
-	
-	return(TRUE);
-}
-
-/* =======================================================
-
       Join and Exit Host
       
 ======================================================= */
 
 bool net_client_join_host_start(char *ip,char *name,int *remote_uid,char *game_name,char *map_name,int *tick_offset,int *option_flags,char *deny_reason,network_reply_join_remotes *remotes)
 {
+	int						len;
+	unsigned char			data[net_max_msg_size];
 	char					err_str[256];
+	network_header			head;
 	network_request_join	request_join;
 	network_reply_join		reply_join;
 	
 		// create client socket
 		
-	client_socket=net_open_tcp_socket();
+	client_socket=net_open_udp_socket();
 	if (client_socket==D3_NULL_SOCKET) {
 		strcpy(err_str,"Networking: Unable to open socket");
 		return(FALSE);
@@ -186,7 +86,7 @@ bool net_client_join_host_start(char *ip,char *name,int *remote_uid,char *game_n
 
 		// connect
 		
-	if (!net_connect_block(client_socket,ip,net_port_host,client_timeout_wait_seconds,err_str)) {
+	if (!net_connect(client_socket,ip,net_port_host,client_timeout_wait_seconds,err_str)) {
 		strcpy(deny_reason,"Host Unreachable (No connection)");
 		net_close_socket(&client_socket);
 		return(FALSE);
@@ -200,11 +100,28 @@ bool net_client_join_host_start(char *ip,char *name,int *remote_uid,char *game_n
 	request_join.tint_color_idx=(signed short)ntohs((short)setup.network.tint_color_idx);
 	request_join.character_idx=(signed short)ntohs((short)setup.network.character_idx);
 	
-	if (!net_queue_block_single_message(client_socket,net_action_request_join,net_remote_uid_none,(unsigned char*)&request_join,sizeof(network_request_join),net_action_reply_join,(unsigned char*)&reply_join,sizeof(network_reply_join))) {
-		strcpy(deny_reason,"Host Unreachable (Bad send/receive)");
+	net_send_message(client_socket,net_action_request_join,net_remote_uid_none,(unsigned char*)&request_join,sizeof(network_request_join));
+
+		// get the reply
+	
+	net_socket_blocking(client_socket,TRUE);
+	len=recv(client_socket,data,net_max_msg_size,0);
+	if (len<=0) {
+		strcpy(deny_reason,"Host Unreachable (Bad Read)");
 		net_close_socket(&client_socket);
 		return(FALSE);
 	}
+
+		// right message?
+
+	memmove(&head,data,sizeof(network_header));
+	if (head.action!=net_action_reply_join) {
+		strcpy(deny_reason,"Host Unreachable (Bad Return)");
+		net_close_socket(&client_socket);
+		return(FALSE);
+	}
+
+	memmove(&reply_join,(data+sizeof(network_header)),sizeof(network_reply_join));
 	
 		// check for denial
 		
