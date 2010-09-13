17e5bb124c5ef2a139e6aa38f8ce2578be28fb7e
ggadwa <ggadwa@2c4a6428-2a2a-0410-ad13-7dde452bfe2e>
2010-09-13 20:47:30 +0000

    09_13_2010 PC

--- dim3Engine/Sources_Server/obj_move.c
+++ dim3Engine/Sources_Server/obj_move.c
@@ -789,8 +789,8 @@ bool object_move_xz_slide(obj_type *obj,int *xadd,int *yadd,int *zadd)
 		obj->pnt.z+=zadd2;
 		return(FALSE);
 	}
-	
-	return(TRUE);	// supergumba -- testing
+
+	return(TRUE);		// supergumba -- do we even need slighting anymore?
 
 		// if the hit poly was wall-like, then find vector for wall
 		// and attempt to slide across it
