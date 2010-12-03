6839ed00965dca6e4cb862a0b12d36f67990ee05
ggadwa <ggadwa@2c4a6428-2a2a-0410-ad13-7dde452bfe2e>
2010-12-03 21:39:48 +0000

    12_03_2010 PC

--- trunk/dim3Editor/Code/property_palette_node.c
+++ trunk/dim3Editor/Code/property_palette_node.c
@@ -36,8 +36,8 @@ and can be sold or given away.
 
 #define kNodePropertyName					0
 #define kNodePropertyEventID				1
-#define kNodePropertyFollowCamera			2
-#define kNodePropertyAlpha					3
+#define kNodePropertyAlpha					2
+#define kNodePropertyFollowCamera			3
 
 extern map_type					map;
 extern editor_state_type		state;
@@ -57,44 +57,13 @@ void property_palette_fill_node(int node_idx)
 
 	node=&map.nodes[node_idx];
 
-	/*
-			dialog_set_text(palette_node_wind,kNodeName,0,node->name);
-	dialog_set_int(palette_node_wind,kNodeEventID,0,node->event_id);
-	dialog_set_boolean(palette_node_wind,kNodeFollowCamera,0,node->follow_camera);
-	dialog_set_float(palette_node_wind,kNodeAlpha,0,node->alpha);
-*/
-	/*
-	list_palette_add_header(&property_palette,0,"Liquid Settings");
-	list_palette_add_checkbox(&property_palette,kLiquidPropertyWaveFlat,"Draw as Flat Surface",liq->tide.flat);
-	list_palette_add_checkbox(&property_palette,kLiquidPropertyNeverObscure,"Never Obscure",liq->never_obscure);
-	list_palette_add_checkbox(&property_palette,kLiquidPropertyNoDraw,"No Draw (Volume Only)",liq->no_draw);
+	list_palette_add_header(&property_palette,0,"Node Identity");
+	list_palette_add_string(&property_palette,kNodePropertyName,"Name",node->name);
+	list_palette_add_string_int(&property_palette,kNodePropertyEventID,"Event ID",node->event_id);
+	list_palette_add_string_float(&property_palette,kNodePropertyAlpha,"Alpha",node->alpha);
 
-	list_palette_add_header(&property_palette,0,"Liquid Under");
-	list_palette_add_pick_color(&property_palette,kLiquidPropertyColor,"Color",&liq->col);
-	list_palette_add_string_float(&property_palette,kLiquidPropertyTintAlpha,"Tint Alpha",liq->tint_alpha);
-	list_palette_add_string_int(&property_palette,kLiquidPropertyDepth,"Depth",liq->depth);
-	list_palette_add_string_float(&property_palette,kLiquidPropertySpeedAlter,"Speed Alter",liq->speed_alter);
-	list_palette_add_string(&property_palette,kLiquidPropertySoundName,"Sound",liq->ambient.sound_name);
-
-	list_palette_add_header(&property_palette,0,"Liquid Waves");
-	list_palette_add_string_int(&property_palette,kLiquidPropertyWaveSize,"Wave Size",liq->tide.division);
-	list_palette_add_string_int(&property_palette,kLiquidPropertyTideSize,"Tide Size",liq->tide.high);
-	list_palette_add_string_int(&property_palette,kLiquidPropertyTideRate,"Tide Rate",liq->tide.rate);
-	list_palette_add_string(&property_palette,kLiquidPropertyTideDirection,"Tide Direction",liquid_property_tide_direction_list[liq->tide.direction]);
-	
-	list_palette_add_header(&property_palette,0,"Liquid Harm");
-	list_palette_add_string_int(&property_palette,kLiquidPropertyHarm,"In Damage",liq->harm.in_harm);
-	list_palette_add_string_int(&property_palette,kLiquidPropertyDrownTick,"Drowning Tick",liq->harm.drown_tick);
-	list_palette_add_string_int(&property_palette,kLiquidPropertyDrownHarm,"Drowning Damage",liq->harm.drown_harm);
-
-	list_palette_add_header(&property_palette,0,"Liquid Group");
-	if (liq->group_idx==-1) {
-		list_palette_add_string(&property_palette,kLiquidPropertyGroup,"Group","");
-	}
-	else {
-		list_palette_add_string(&property_palette,kLiquidPropertyGroup,"Group",map.group.groups[liq->group_idx].name);
-	}
-	*/
+	list_palette_add_header(&property_palette,0,"Node Settings");
+	list_palette_add_checkbox(&property_palette,kNodePropertyFollowCamera,"Follow Camera",node->follow_camera);
 }
 
 /* =======================================================
@@ -109,38 +78,21 @@ void property_palette_click_node(int node_idx,int id)
 
 	node=&map.nodes[node_idx];
 
-	/*
 	switch (id) {
 
-		case kLiquidPropertyWaveFlat:
-			liq->tide.flat=!liq->tide.flat;
+		case kNodePropertyName:
+		case kNodePropertyEventID:
 			break;
 
-		case kLiquidPropertyNeverObscure:
-			liq->never_obscure=!liq->never_obscure;
+		case kNodePropertyAlpha:
 			break;
 
-		case kLiquidPropertyNoDraw:
-			liq->no_draw=!liq->no_draw;
-			break;
-
-		case kLiquidPropertyColor:
-		case kLiquidPropertyTintAlpha:
-		case kLiquidPropertyDepth:
-		case kLiquidPropertySpeedAlter:
-		case kLiquidPropertySoundName:
-		case kLiquidPropertyWaveSize:
-		case kLiquidPropertyTideSize:
-		case kLiquidPropertyTideRate:
-		case kLiquidPropertyTideDirection:
-		case kLiquidPropertyHarm:
-		case kLiquidPropertyDrownTick:
-		case kLiquidPropertyDrownHarm:
-		case kLiquidPropertyGroup:
+		case kNodePropertyFollowCamera:
+			node->follow_camera=!node->follow_camera;
 			break;
 
 	}
-*/
+
 	main_wind_draw();
 }
 
