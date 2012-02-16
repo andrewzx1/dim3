0a97e05982f0b40816c4621be1a20906af1aadce
ggadwa <ggadwa@charter.net>
2012-02-16 05:14:18 +0000

    02_16_2012

--- dim3Engine/Sources_View/particles_draw.c
+++ dim3Engine/Sources_View/particles_draw.c
@@ -120,8 +120,7 @@ void particle_draw_position(effect_type *effect,int count,d3pnt *pnt)
       
 ======================================================= */
 
-/* supergumba -- old way
-int particle_fill_array_quad_single(float *vertex_ptr,int idx,int nvertex,d3pnt *pnt,d3ang *rot_ang,float pixel_size,matrix_type *pixel_mat,float gravity,float f_count,int particle_count,iface_particle_piece_type *pps,float gx,float gy,float g_size)
+int particle_fill_array_quad_single(float *vertex_ptr,int idx,int nvertex,d3pnt *pnt,d3ang *rot_ang,float pixel_size,matrix_type *pixel_x_mat,matrix_type *pixel_y_mat,float gravity,float f_count,int particle_count,iface_particle_piece_type *pps,float gx,float gy,float g_size)
 {
 	int					n,k;
 	float				fx,fy,fz,px[4],py[4],pz[4];
@@ -143,7 +142,8 @@ int particle_fill_array_quad_single(float *vertex_ptr,int idx,int nvertex,d3pnt 
 	pz[0]=pz[1]=pz[2]=pz[3]=0.0f;
 	
 	for (k=0;k!=4;k++) {
-		matrix_vertex_multiply(pixel_mat,&px[k],&py[k],&pz[k]);
+		matrix_vertex_multiply_ignore_transform(pixel_x_mat,&px[k],&py[k],&pz[k]);
+		matrix_vertex_multiply_ignore_transform(pixel_y_mat,&px[k],&py[k],&pz[k]);
 	}
 	
 		// fill particle arrays
@@ -159,7 +159,7 @@ int particle_fill_array_quad_single(float *vertex_ptr,int idx,int nvertex,d3pnt 
 			fy=pps->vct.y;
 			fz=pps->vct.z;
 			
-			matrix_vertex_multiply(&rot_mat,&fx,&fy,&fz);
+			matrix_vertex_multiply_ignore_transform(&rot_mat,&fx,&fy,&fz);
 		
 			fx=(float)(pnt->x+pps->pt.x)+(fx*f_count);
 			fy=(float)(pnt->y+pps->pt.y)+((fy*f_count)+gravity);
@@ -235,47 +235,6 @@ int particle_fill_array_quad_single(float *vertex_ptr,int idx,int nvertex,d3pnt 
 	
 	return(idx+(particle_count*6));
 }
-*/
-
-int particle_fill_array_point_single(float *vertex_ptr,int idx,d3pnt *pnt,d3ang *rot_ang,float gravity,float f_count,int particle_count,iface_particle_piece_type *pps)
-{
-	int					n;
-	float				fx,fy,fz;
-	float				*pf;
-	matrix_type			rot_mat;
-	
-		// if move angle, setup matrixes
-		
-	if (rot_ang!=NULL) matrix_rotate_xzy(&rot_mat,rot_ang->x,rot_ang->y,rot_ang->z);
-	
-		// fill particle arrays
-	
-	pf=vertex_ptr+(idx*3);
-	
-	for (n=0;n!=particle_count;n++) {
-	
-		if (rot_ang!=NULL) {			// rotated particles
-			fx=pps->vct.x;
-			fy=pps->vct.y;
-			fz=pps->vct.z;
-			
-			matrix_vertex_multiply(&rot_mat,&fx,&fy,&fz);
-		
-			*pf++=(float)(pnt->x+pps->pt.x)+(fx*f_count);
-			*pf++=(float)(pnt->y+pps->pt.y)+((fy*f_count)+gravity);
-			*pf++=(float)(pnt->z+pps->pt.z)+(fz*f_count);
-		}
-		else {							// normal particles
-			*pf++=(float)(pnt->x+pps->pt.x)+((pps->vct.x)*f_count);
-			*pf++=(float)(pnt->y+pps->pt.y)+(((pps->vct.y)*f_count)+gravity);
-			*pf++=(float)(pnt->z+pps->pt.z)+((pps->vct.z)*f_count);
-		}
-		
-		pps++;
-	}
-	
-	return(idx+(particle_count*3));
-}
 
 /* =======================================================
 
@@ -289,15 +248,14 @@ void particle_draw(effect_type *effect,int count)
 							ntrail,pixel_dif;
 	float					gravity,gx,gy,g_size,pixel_sz,f,pc[3],trail_step,
 							alpha,alpha_dif,color_dif,f_count,f_tick;
-	float					point_attn[3]={0.0f,1.0f,0.0f};
 	float					*vertex_ptr;
 	d3pnt					pnt;
 	d3ang					*rot_ang,rang;
 	d3col					col,ambient_col;
 	iface_particle_type		*particle;
 	particle_effect_data	*eff_particle;
-	matrix_type				pixel_mat;
-
+	matrix_type				pixel_x_mat,pixel_y_mat;
+	
 	eff_particle=&effect->data.particle;
 	particle=&iface.particle_list.particles[eff_particle->particle_idx];
 
@@ -349,10 +307,10 @@ void particle_draw(effect_type *effect,int count)
 		rot_ang->z=angle_add(rot_ang->z,f);
 	}
 
-		// particle sprite rotation
-
-	// supergumba -- get rid of this!
-	matrix_rotate_xy(&pixel_mat,view.render->camera.ang.x,view.render->camera.ang.y);
+		// particle billboard rotation
+		
+	matrix_rotate_x(&pixel_x_mat,view.render->camera.ang.x);
+	matrix_rotate_y(&pixel_y_mat,view.render->camera.ang.y);
 	
 		// reverse
 		
@@ -408,14 +366,8 @@ void particle_draw(effect_type *effect,int count)
 		// effect vbos are dynamic, so it'll auto construct
 		// the first time called
 
-/* supergumba -- old way
 	nvertex=(particle->count*(particle->trail_count+1))*6;
 	view_create_effect_vertex_object(effect,((nvertex*(3+2))*sizeof(float)));
-*/
-
-	nvertex=particle->count*(particle->trail_count+1);
-
-	view_create_effect_vertex_object(effect,((nvertex*3)*sizeof(float)));
 
 	view_bind_effect_vertex_object(effect);
 	vertex_ptr=(float*)view_map_effect_vertex_object();
@@ -442,21 +394,14 @@ void particle_draw(effect_type *effect,int count)
 
 			// draw pixels
 
-		/* supergumba -- old way
-		idx=particle_fill_array_quad_single(vertex_ptr,idx,nvertex,&pnt,rot_ang,pixel_sz,&pixel_mat,gravity,f_count,particle_count,particle->pieces[eff_particle->variation_idx],gx,gy,g_size);
-		*/
-		idx=particle_fill_array_point_single(vertex_ptr,idx,&pnt,rot_ang,gravity,f_count,particle_count,particle->pieces[eff_particle->variation_idx]);
+		idx=particle_fill_array_quad_single(vertex_ptr,idx,nvertex,&pnt,rot_ang,pixel_sz,&pixel_x_mat,&pixel_y_mat,gravity,f_count,particle_count,particle->pieces[eff_particle->variation_idx],gx,gy,g_size);
 
 			// reduce pixel sizes and counts for trails
 			
-	/* supergumba -- old way, this is now done when drawing
 		pixel_sz=pixel_sz*particle->reduce_pixel_fact;
-	*/
 		f_count-=trail_step;
 	}
 
-
-/* supergumba -- old way
 		// unmap vertex object
 
 	view_unmap_effect_vertex_object();
@@ -494,122 +439,6 @@ void particle_draw(effect_type *effect,int count)
 	glDepthMask(GL_TRUE);
 	
 	gl_texture_simple_end();
-
-		// unbind vertex object
-		
-	view_unbind_effect_vertex_object();
-*/
-
-		// unmap vertex object
-
-	view_unmap_effect_vertex_object();
-
-		// enable point sprites
-
-	point_attn[0]=0.0f;
-	point_attn[1]=1.0f;
-	point_attn[2]=0.0f;
-
-	point_attn[0]=1.0f;
-	point_attn[1]=0.0f;
-	point_attn[2]=0.0f;
-
-	//GLfloat fSizes[2];
-	//glGetFloatv(GL_ALIASED_POINT_SIZE_RANGE, fSizes);
-
-	glEnable(GL_POINT_SMOOTH);	// supergumba!
-
-	glPointParameterf(GL_POINT_FADE_THRESHOLD_SIZE_ARB,0.0f);
-	glPointParameterf(GL_POINT_SIZE_MIN_ARB,1.0f);
-	glPointParameterf(GL_POINT_SIZE_MAX_ARB,8000.0f);
-	glPointParameterfv(GL_POINT_DISTANCE_ATTENUATION,point_attn);
-	glTexEnvi(GL_POINT_SPRITE,GL_COORD_REPLACE,GL_TRUE);
-
-	glEnable(GL_POINT_SPRITE);
-	
-		// draw arrays
-		
-	gl_texture_simple_start();
-	gl_texture_simple_set(view_images_get_gl_id(particle->image_idx),FALSE,col.r,col.g,col.b,alpha);
-
-	glEnable(GL_BLEND);
-
-	if (particle->blend_add) {
-		glBlendFunc(GL_SRC_ALPHA,GL_ONE);
-	}
-	else {
-		glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
-	}
-
-	glEnable(GL_ALPHA_TEST);
-	glAlphaFunc(GL_NOTEQUAL,0);
-
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-	glDepthMask(GL_FALSE);			// don't let alpha z's interfere with each other
-
-	glVertexPointer(3,GL_FLOAT,(3*sizeof(float)),(GLvoid*)0);
-
-		// do each trail separately
-		// so we can change any animated
-		// textures and point sizes
-
-	idx=0;
-			
-	for (n=0;n!=ntrail;n++) {
-
-			// pixel size for this trail
-
-		glPointSize(pixel_sz*100);
-
-			// if a sheet of sprites,
-			// then setup the texture offsets
-
-		if (g_size!=1.0f) {
-			glMatrixMode(GL_TEXTURE);
-			glTranslatef(gx,gy,0.0f);
-			glScalef(g_size,g_size,1.0f);
-		}
-
-			// draw the points
-
-		glDrawArrays(GL_POINTS,idx,particle->count);
-		idx+=particle->count;
-	
-			// change particle image
-		
-		if (g_size!=1.0f) {
-		
-			if (gx<1.0f) {
-				gx+=g_size;
-			}
-			else {
-				gx=0.0f;
-				gy+=g_size;
-				if (gy>=1.0f) gy=0.0f;
-			}
-		}
-
-			// change particle size
-
-		pixel_sz*=particle->reduce_pixel_fact;
-	}
-
-		// reset any texture changes
-
-	if (g_size!=1.0f) {
-		glMatrixMode(GL_TEXTURE);
-		glLoadIdentity();
-	}
-
-	gl_texture_simple_end();
-
-	glDisable(GL_POINT_SPRITE);
-
-		// reset opengl settings
-
-	glPointSize(1.0f);
-	glDepthMask(GL_TRUE);
 
 		// unbind vertex object
 		
