2f6c1f21c8e0e2145144fde0f6504a7f06f879d8
ggadwa <ggadwa@2c4a6428-2a2a-0410-ad13-7dde452bfe2e>
2012-08-06 21:00:16 +0000

    08_06_2012

--- trunk/winBuild/Common/Code/win32_dialog_property_string.c
+++ trunk/winBuild/Common/Code/win32_dialog_property_string.c
@@ -49,6 +49,84 @@ int								dialog_property_string_value_type,
 								dialog_property_string_i_max;
 void							*dialog_property_string_value;
 
+
+
+
+
+#define os_dialog_msg_type_init				0
+#define os_dialog_msg_type_button			1
+
+HWND		os_dialog_wind;
+void*		os_dialog_callback;
+
+LRESULT CALLBACK os_dialog_proc(HWND diag,UINT msg,WPARAM wparam,LPARAM lparam)
+{
+	switch (msg) {
+
+	//	case WM_NCCREATE:
+	//		return(TRUE);
+
+	//	case WM_INITDIALOG:
+	//		return(FALSE);		// return false when keyboard focus has been set
+
+		case WM_COMMAND:
+
+
+			break;
+
+		default:
+			return(DefWindowProc(diag,msg,wparam,lparam));
+	}
+
+	return(0);
+}
+
+void os_dialog_create(char *title,int wid,int high,void *callback)
+{
+	WNDCLASSEX			wcx;
+
+		// setup callback
+
+	os_dialog_callback=callback;
+
+		// create window
+
+    wcx.cbSize=sizeof(wcx);
+    wcx.style=CS_DBLCLKS;
+    wcx.lpfnWndProc=os_dialog_proc;
+    wcx.cbClsExtra=0;
+    wcx.cbWndExtra=0;
+    wcx.hInstance=hinst;
+    wcx.hIcon=NULL;
+    wcx.hCursor=LoadCursor(NULL,IDC_ARROW);
+    wcx.hbrBackground=(HBRUSH)GetStockObject(WHITE_BRUSH);
+    wcx.lpszMenuName=NULL;
+    wcx.lpszClassName="dim3DialogClass";
+    wcx.hIconSm=NULL;
+
+    RegisterClassEx(&wcx);
+
+    os_dialog_wind=CreateWindowEx(WS_EX_DLGMODALFRAME,"dim3DialogClass",title,WS_POPUP|WS_CAPTION|WS_SYSMENU,20,20,wid,high,wnd,NULL,hinst,NULL);
+	EnableWindow(wnd,FALSE);
+}
+
+void os_dialog_show(void)
+{
+	ShowWindow(os_dialog_wind,SW_SHOWNORMAL);
+}
+
+void os_dialog_close(void)
+{
+	EnableWindow(wnd,TRUE);
+	DestroyWindow(os_dialog_wind);
+	UnregisterClass("dim3DialogClass",GetModuleHandle(NULL));
+}
+
+
+
+
+
+
 /* =======================================================
 
       Property String Event Handlers
@@ -61,14 +139,18 @@ LRESULT CALLBACK dialog_property_string_proc(HWND diag,UINT msg,WPARAM wparam,LP
 
 	switch (msg) {
 
+		case WM_NCCREATE:
+			return(TRUE);
+
 		case WM_INITDIALOG:
 
-			property_string_get_values(dialog_property_string_value_type,dialog_property_string_value,dialog_property_string_i_min,dialog_property_string_i_max,str,desc);
-			win32_dialog_set_text(diag,IDC_PROPERTY_STRING_STRING,str);
-			SetWindowText(diag,desc);
+		//	property_string_get_values(dialog_property_string_value_type,dialog_property_string_value,dialog_property_string_i_min,dialog_property_string_i_max,str,desc);
+		//	win32_dialog_set_text(diag,IDC_PROPERTY_STRING_STRING,str);
+		//	SetWindowText(diag,desc);
 
-			win32_dialog_set_focus(diag,IDC_PROPERTY_STRING_STRING);
-			win32_dialog_select_all(diag,IDC_PROPERTY_STRING_STRING);
+		//	win32_dialog_set_focus(diag,IDC_PROPERTY_STRING_STRING);
+		//	win32_dialog_select_all(diag,IDC_PROPERTY_STRING_STRING);
+
 
 			return(FALSE);		// return false when keyboard focus has been set
 
@@ -77,9 +159,11 @@ LRESULT CALLBACK dialog_property_string_proc(HWND diag,UINT msg,WPARAM wparam,LP
 			switch (LOWORD(wparam)) {
 
 				case ID_PROPERTY_STRING_OK:
-					win32_dialog_get_text(diag,IDC_PROPERTY_STRING_STRING,str,256);
-					property_string_set_values(dialog_property_string_value_type,dialog_property_string_value,dialog_property_string_value_len,dialog_property_string_i_min,dialog_property_string_i_max,str);
-					EndDialog(diag,0);
+				//	win32_dialog_get_text(diag,IDC_PROPERTY_STRING_STRING,str,256);
+				//	property_string_set_values(dialog_property_string_value_type,dialog_property_string_value,dialog_property_string_value_len,dialog_property_string_i_min,dialog_property_string_i_max,str);
+				//	EndDialog(diag,0);
+
+					os_dialog_close();
 					return(TRUE);
 
 			}
@@ -97,6 +181,19 @@ LRESULT CALLBACK dialog_property_string_proc(HWND diag,UINT msg,WPARAM wparam,LP
       
 ======================================================= */
 
+void dialog_property_string_proc_2(int msg_type,int button_id)
+{
+	switch (msg_type) {
+
+		case os_dialog_msg_type_button:
+			if (button_id==ID_PROPERTY_STRING_OK) {
+				os_dialog_close();
+			}
+			break;
+
+	}
+}
+
 void dialog_property_string_run(int value_type,void *value,int value_len,int i_min,int i_max)
 {
 	dialog_property_string_value_type=value_type;
@@ -105,5 +202,8 @@ void dialog_property_string_run(int value_type,void *value,int value_len,int i_m
 	dialog_property_string_i_min=i_min;
 	dialog_property_string_i_max=i_max;
 
-	DialogBox(hinst,MAKEINTRESOURCE(IDD_PROPERTY_STRING),wnd,dialog_property_string_proc);
+//	DialogBox(hinst,MAKEINTRESOURCE(IDD_PROPERTY_STRING),wnd,dialog_property_string_proc);
+
+	os_dialog_create("Test",500,200,dialog_property_string_proc_2);
+	os_dialog_show();
 }
