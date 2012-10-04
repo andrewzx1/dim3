d87b1b65de4b86007830544bf8f57486f9963629
ggadwa <ggadwa@charter.net>
2012-10-04 04:42:08 +0000

    10_04_2012

--- dim3Engine/Sources_OpenRL/ray_os_defs.h
+++ dim3Engine/Sources_OpenRL/ray_os_defs.h
@@ -1,28 +1,17 @@
-#ifdef OSX
-
-	#include <carbon/carbon.h>
+#ifdef __APPLE__
 
 	#include <stdio.h>
 	#include <stdlib.h>
-	#include <string.h>
 	#include <sys/stat.h>
 	#include <sys/param.h>
 	#include <sys/sysctl.h>
 	#include <unistd.h>
-	#include <dirent.h>
 
 	#include <pthread.h>
 
-	#include <OpenGL/OpenGL.h>
-	#include <OpenGL/gl.h>
-	#include <OpenGL/glext.h>
-	#include <OpenGL/glu.h>
-
-	#include <zlib.h>
-
 #endif
 
-#ifdef LINUX
+#ifdef __linux__
 
 	#include <unistd.h>
 
@@ -44,19 +33,9 @@
 	#include <time.h>
 	#include <process.h>
 
-	#include "glew.h"
-	#include "wglew.h"
-
 	#define inline					__inline
 
 	typedef unsigned char			bool;
-
-	#define bzero(a,b)				memset(a,0x0,(b))
-	#define bcmp(a,b,c)				memcmp(a,b,(c))
-	#define usleep(x)               Sleep((DWORD)((x)/1000))
-	
-	#define random					rand
-	#define srandom					srand
 
 #endif
 
