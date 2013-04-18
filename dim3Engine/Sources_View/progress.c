3c7d5b3ec365c001d2e779a49278bdb139c16a7a
ggadwa <ggadwa@2c4a6428-2a2a-0410-ad13-7dde452bfe2e>
2013-04-18 20:53:34 +0000

    04_18_2013

--- dim3Engine/Sources_View/progress.c
+++ dim3Engine/Sources_View/progress.c
@@ -38,7 +38,7 @@ extern iface_type			iface;
 extern setup_type			setup;
 extern file_path_setup_type	file_path_setup;
 
-int							progress_start_tick;
+int							progress_start_tick,progress_cur_tick;
 bool						progress_on;
 bitmap_type					progress_background_bitmap,progress_bitmap;
 
@@ -56,6 +56,18 @@ void progress_initialize(char *map_name)
 		// ignore if dedicated host
 
 	if (app.dedicated_host) return;
+	
+		// current progress
+		
+	progress_start_tick=-1;
+	progress_cur_tick=0;
+
+		// ray trace version
+
+	if (iface.project.ray_trace) {
+		progress_dim3rtl_initialize(map_name);
+		return;
+	}
 	
 		// check for map progress background,
 		// otherwise use default
@@ -80,10 +92,6 @@ void progress_initialize(char *map_name)
 		file_paths_data(&file_path_setup,path,"Bitmaps/Interface",iface.progress.bitmap_name,"png");
 		progress_on=bitmap_open(&progress_bitmap,path,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE);
 	}
-	
-		// current progress
-		
-	progress_start_tick=-1;
 }
 
 void progress_shutdown(void)
@@ -91,6 +99,13 @@ void progress_shutdown(void)
 		// ignore if dedicated host
 
 	if (app.dedicated_host) return;
+
+		// ray trace version
+
+	if (iface.project.ray_trace) {
+		progress_dim3rtl_shutdown();
+		return;
+	}
 
 		// close bitmaps
 
@@ -106,47 +121,67 @@ void progress_shutdown(void)
 
 void progress_update(void)
 {
-	int				lft,rgt,top,bot;
+	int				lft,rgt,top,bot,
+					time_tick;
 	float			gx,gy,g_size;
 
 		// ignore if dedicated host
 
 	if (app.dedicated_host) return;
 	
+		// first time, get timing info
+		// using direct timer as game is paused
+		
+	if (progress_start_tick==-1) progress_start_tick=time_get();
+
+		// we don't want to skip frames
+		// so we cheat with the progress timing
+
+	time_tick=time_get();
+
+	if ((time_tick-progress_start_tick)>=iface.progress.animate.msec) {
+		progress_cur_tick+=iface.progress.animate.msec;
+		progress_start_tick=time_tick+((time_tick-progress_start_tick)%iface.progress.animate.msec);
+	}
+	
 		// start the frame
 	
 	gl_frame_clear(FALSE);
 	gl_shader_frame_start();
-	
-		// setup draw
-		
-	gl_2D_view_interface();
-	
-		// draw background
-		// progress can be called while baseutility loads bitmaps, need to reset the current bitmap so it doesn't get lost
 
-	gl_texture_clear(0);
-	view_primitive_2D_texture_quad(progress_background_bitmap.gl_id,NULL,1.0f,0,iface.scale_x,0,iface.scale_y,0.0f,1.0f,0.0f,1.0f,TRUE);
-	
-		// draw the progress bitmap
-		
-	if (progress_on) {
-	
-			// first time, get timing info
-			// using direct timer as game is paused
-			
-		if (progress_start_tick==-1) progress_start_tick=time_get();
+		// opengl progress
 
-			// draw the graphic
-			
-		lft=iface.progress.x;
-		rgt=lft+iface.progress.wid;
-		top=iface.progress.y;
-		bot=top+iface.progress.high;
-	
-		effect_image_animate_get_uv((time_get()-progress_start_tick),0,&iface.progress.animate,&gx,&gy,&g_size);
+	if (!iface.project.ray_trace) {
 		
-		view_primitive_2D_texture_quad(progress_bitmap.gl_id,NULL,1.0f,lft,rgt,top,bot,gx,(gx+g_size),gy,(gy+g_size),TRUE);
+		gl_2D_view_interface();
+	
+			// draw background
+			// progress can be called while baseutility loads bitmaps, need to reset the current bitmap so it doesn't get lost
+
+		gl_texture_clear(0);
+		view_primitive_2D_texture_quad(progress_background_bitmap.gl_id,NULL,1.0f,0,iface.scale_x,0,iface.scale_y,0.0f,1.0f,0.0f,1.0f,TRUE);
+		
+			// draw the progress bitmap
+			
+		if (progress_on) {
+
+				// draw the graphic
+				
+			lft=iface.progress.x;
+			rgt=lft+iface.progress.wid;
+			top=iface.progress.y;
+			bot=top+iface.progress.high;
+		
+			effect_image_animate_get_uv(progress_cur_tick,0,&iface.progress.animate,&gx,&gy,&g_size);
+			
+			view_primitive_2D_texture_quad(progress_bitmap.gl_id,NULL,1.0f,lft,rgt,top,bot,gx,(gx+g_size),gy,(gy+g_size),TRUE);
+		}
+	}
+
+		// dim3rtl progress
+
+	else {
+		progress_dim3rtl_draw(progress_cur_tick);
 	}
 
 	gl_frame_swap();
